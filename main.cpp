a5e4ea000a74ffd9e5f618f122af5823e07c516e
ReginaCsaszar <regina.csaszar@gmail.com>
2017-10-16 16:37:18 +0200

    Refactor: - use const chars in namespace, - separate tables in the class: one for the solution and another for the revealed steps, - filling arrays with memset, instead of a for loop - filling table with mines using a while loop instead of for loop.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,84 @@
 #include <iostream>
 #include <random>
+#include <cstring>
 
 namespace {
 
-    char mine = '*';
-    char empty = '.';
-    char space = '0';
+    const char mine = '*';
+    const char space = ' ';
 
     class Minesweeper {
+
     public:
         Minesweeper(const size_t width, const size_t height, const int bomb)
                 : width(width), height(height), mines(bomb),
-                  table(new char[width * height]) {
+                  table(new char[width * height]), playerTable(new char[width * height]) {
             fillTable();
         }
 
-        /* In a real implementation there would also be a
-         * - copy constructor
-         * - assignment operator
-         * - move constructor
-         * - move assignment operator
-         * We will learn about them later
-         */
-
-
-
         virtual ~Minesweeper() {
             delete[] table;
+            delete[] playerTable;
+        }
+
+        void printTable() const {
+            for (int y = 0; y < height; y++) {
+                for(int x = 0; x < width; x++) { std::cout << table[y*width+x] <<" "; }
+                std::cout << std::endl;
+            }
+        }
+
+        void reveal(int x, int y) {
+
+            int index = y*width+x;
+            char *field = &table[index];
+
+            if ( *field == mine) {
+                std::cout <<"\nBOOOM! YOU DIED!"<<std::endl;
+
+            } else if ( *field == '0') {
+                char *revealTable = new char[width * height];
+
+                revealTable[index] = *field;
+                revealNeighboors(x, y);
+
+                std::cout<<"\nThe reveal table content:\n\n";
+                for(int y = 0; y < height; y++) {
+                    for(int x = 0; x < width; x++) {
+                        std::cout <<revealTable[y*width+x]<<" "; }
+                    std::cout<<std::endl; }
+                delete[] revealTable;
+
+            } else {
+                std::cout <<"You revealed a number "<< *field <<std::endl;
+            }
+
+
+        }
+
+    private:
+
+        void fillTable() {
+
+            size_t size = width * height;
+
+            memset(table, space, size);
+
+            std::random_device rd;
+            std::mt19937_64 gen(rd());
+            std::uniform_int_distribution<size_t> randIndex(0, size);
+
+            unsigned mineCount = 0;
+
+            do {
+                size_t place = randIndex(gen);
+                if(table[place] != mine) {
+                    table[place] = mine;
+                    mineCount++;
+                }
+            } while (mineCount < mines);
+
+            countNeighbours();
         }
 
         void countNeighbours() {
@@ -59,105 +111,44 @@ namespace {
             }
         }
 
-        void printTable() const {
-            std::cout<<"\nThe table content:\n\n";
-            for(int y = 0; y < height; y++)
-            {
-                for(int x = 0; x < width; x++)
-                {
-                    std::cout <<table[y*width+x]<<" ";
-                }
-                std::cout<<std::endl;
+        void revealNeighboors(size_t x, size_t y) {
+
+            if ((table[(y-1)*width+x] == space)&&(playerTable[(y-1)*width+x] != space)) {
+                playerTable[(y-1)*width+x] = space;
+                revealNeighboors(x, y-1);
             }
-
-        }
-
-        void reveal(int x, int y) {
-
-            int index = y*width+x;
-            char *field = &table[index];
-
-            if ( *field == mine) {
-                std::cout <<"\nBOOOM! YOU DIED!"<<std::endl;
-
-            } else if ( *field == '0') {
-                char *revealTable = new char[width * height];
-
-                revealTable[index] = *field;
-                revealNeighboors(x, y, revealTable);
-
-                std::cout<<"\nThe reveal table content:\n\n";
-                for(int y = 0; y < height; y++) {
-                    for(int x = 0; x < width; x++) {
-                        std::cout <<revealTable[y*width+x]<<" "; }
-                    std::cout<<std::endl; }
-                delete[] revealTable;
-
-            } else {
-                std::cout <<"You revealed a number "<< *field <<std::endl;
-            }
-
-
-        }
-
-    private:
-        void fillTable() {
-
-            for(int i = 0; i < height*width; i++) {
-                table[i] = empty;
-            }
-
-            std::random_device rd;
-            std::mt19937_64 gen(rd());
-            std::uniform_int_distribution<size_t> randIndex(0, width*height);
-
-            for(int i = 0; i < mines; i++) {
-                size_t m = randIndex(gen);
-                if(table[m] == mine) {
-                    i--;
-                } else {
-                    table[m] = mine; }
-            }
-        }
-
-        void revealNeighboors(size_t x, size_t y, char *revealTable) {
-
-            if ((table[(y-1)*width+x] == space)&&(revealTable[(y-1)*width+x] != space)) {
-                revealTable[(y-1)*width+x] = space;
-                revealNeighboors(x, y-1, revealTable);
-            }
-            if ((table[(y+1)*width+x] == space)&&(revealTable[(y+1)*width+x] != space)) {
-                revealTable[(y+1)*width+x] = space;
-                revealNeighboors(x, y+1, revealTable);
+            if ((table[(y+1)*width+x] == space)&&(playerTable[(y+1)*width+x] != space)) {
+                playerTable[(y+1)*width+x] = space;
+                revealNeighboors(x, y+1);
             }
 
             if (x > 0) {
-                if ((table[(y-1)*width+x-1] == space)&&(revealTable[(y-1)*width+x-1] != space)) {
-                    revealTable[(y-1)*width+x-1] = space;
-                    revealNeighboors(x-1, y-1, revealTable);
+                if ((table[(y-1)*width+x-1] == space)&&(playerTable[(y-1)*width+x-1] != space)) {
+                    playerTable[(y-1)*width+x-1] = space;
+                    revealNeighboors(x-1, y-1);
                 }
-                if ((table[y*width+x-1] == space)&&(revealTable[y*width+x-1] != space)) {
-                    revealTable[y*width+x-1] = space;
-                    revealNeighboors(x-1, y, revealTable);
+                if ((table[y*width+x-1] == space)&&(playerTable[y*width+x-1] != space)) {
+                    playerTable[y*width+x-1] = space;
+                    revealNeighboors(x-1, y);
                 }
-                if ((table[(y+1)*width+x-1] == space)&&(revealTable[(y+1)*width+x-1] != space)) {
-                    revealTable[(y+1)*width+x-1] = space;
-                    revealNeighboors(x-1, y+1, revealTable);
+                if ((table[(y+1)*width+x-1] == space)&&(playerTable[(y+1)*width+x-1] != space)) {
+                    playerTable[(y+1)*width+x-1] = space;
+                    revealNeighboors(x-1, y+1);
                 }
             }
 
             if (x < width-1) {
-                if ((table[(y-1)*width+x+1]== space)&&(revealTable[(y-1)*width+x+1] != space)) {
-                    revealTable[(y-1)*width+x+1]= space;
-                    revealNeighboors(x+1, y-1, revealTable);
+                if ((table[(y-1)*width+x+1]== space)&&(playerTable[(y-1)*width+x+1] != space)) {
+                    playerTable[(y-1)*width+x+1]= space;
+                    revealNeighboors(x+1, y-1);
                 }
-                if ((table[y*width+x+1]== space)&&(revealTable[y*width+x+1] != space)) {
-                    revealTable[y*width+x+1] = space;
-                    revealNeighboors(x+1, y, revealTable);
+                if ((table[y*width+x+1]== space)&&(playerTable[y*width+x+1] != space)) {
+                    playerTable[y*width+x+1] = space;
+                    revealNeighboors(x+1, y);
                 }
-                if ((table[(y+1)*width+x+1]== space)&&(revealTable[(y+1)*width+x+1] != space)) {
-                    revealTable[(y+1)*width+x+1] = space;
-                    revealNeighboors(x+1, y+1, revealTable);
+                if ((table[(y+1)*width+x+1]== space)&&(playerTable[(y+1)*width+x+1] != space)) {
+                    playerTable[(y+1)*width+x+1] = space;
+                    revealNeighboors(x+1, y+1);
 
                 }
             }
@@ -165,7 +156,8 @@ namespace {
 
         const size_t width, height;
         const int mines;
-        char *table;
+        char* table;
+        char* playerTable;
     };
 }
 
@@ -173,9 +165,6 @@ int main() {
     try {
         Minesweeper ms(10, 10, 15);
         ms.printTable();
-        ms.countNeighbours();
-        ms.printTable();
-        ms.reveal(5, 5);
     } catch (const std::bad_alloc &e) {
         std::cerr << "Couldn't allocate enough memory for minesweeper table" << std::endl;
         return EXIT_FAILURE;
